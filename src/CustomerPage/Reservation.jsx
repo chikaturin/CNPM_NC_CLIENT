a8076ed8ac73c322df22281a5f248c1ec76d8701
Chikaturin <ninhhoaithanh@gmail.com>
2024-11-06 14:08:31 +0700

    "Updated Reservation and CarDetail components to use react-calendar, added calendar functionality, and refactored code to handle date picking and validation."

--- src/CustomerPage/Reservation.jsx
+++ src/CustomerPage/Reservation.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faXmark,
@@ -21,14 +23,19 @@ const Reservation = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   const [Null, setNull] = useState(null);
+  const [Null2, setNull2] = useState(null);
   const [PickupDate, setPickUpDate] = useState(null);
   const [ReturnDate, setReturnDate] = useState(null);
+  const [StartDate, setStartDate] = useState(null);
+  const [DesiredDate, setDesiredDate] = useState(null);
+  const [EndDate, setEndDate] = useState(null);
+  const [showPickupCalendar, setShowPickupCalendar] = useState(false);
+  const [showReturnCalendar, setShowReturnCalendar] = useState(false);
   const [insurance] = useState(82400);
   const [total, setTotal] = useState(0);
-  const today = new Date();
-  const selectedReturnDate = new Date(ReturnDate);
-  const selectedPickUpDate = new Date(PickupDate);
   const navigate = useNavigate();
+  const URL = "https://cnpm-ncserver.vercel.app/api";
+
   const formatDate = (a) => {
     return new Date(a).toLocaleDateString("en-GB", {
       day: "2-digit",
@@ -36,22 +43,112 @@ const Reservation = () => {
       year: "numeric",
     });
   };
-  const URL = "https://cnpm-ncserver.vercel.app/api";
+
+  const togglePickupCalendar = (e) => {
+    e.preventDefault();
+    setShowPickupCalendar(!showPickupCalendar);
+  };
+
+  const toggleReturnCalendar = (e) => {
+    e.preventDefault();
+    setShowReturnCalendar(!showReturnCalendar);
+  };
+
+  const handlPickupDateChange = (date) => {
+    setPickUpDate(date);
+    setShowPickupCalendar(!showPickupCalendar);
+  };
+
+  const handleReturnDateChange = (date) => {
+    setReturnDate(date);
+    setShowReturnCalendar(!showReturnCalendar);
+  };
+
+  const fetchCalculate = async () => {
+    try {
+      const response = await fetch(`${URL}/CalculateContractPrice`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          Pickup_Date: new Date(PickupDate),
+          Return_Date: new Date(ReturnDate),
+          Insurance: insurance,
+          MaVehicle: id,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setTotal(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (ReturnDate >= DesiredDate && PickupDate < DesiredDate) {
+      let desiredDate = new Date(DesiredDate);
+      desiredDate.setDate(desiredDate.getDate() + 1);
+      const date = formatDate(desiredDate);
+      setNull2("Vui lòng chọn ngày trả xe sau ngày " + date);
+    } else if (ReturnDate && PickupDate && ReturnDate <= PickupDate) {
+      setNull2("Vui lòng chọn ngày trả xe sau ngày nhận xe");
+    } else if (ReturnDate && PickupDate && PickupDate < ReturnDate) {
+      setNull(null);
+      setNull2(null);
+      fetchCalculate();
+    }
+  }, [PickupDate, ReturnDate]);
+
+  const fetchCalendar = async () => {
+    try {
+      const response = await fetch(`${URL}/datereservation/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      if (data.StartDate) {
+        const [day, month, year] = data.StartDate.split("/");
+        const formattedDate = new Date(`${year}-${month}-${day}`);
+        setStartDate(formattedDate);
+      }
+      if (data.DesiredDate) {
+        const [day, month, year] = data.DesiredDate.split("/");
+        const formattedDate = new Date(`${year}-${month}-${day}`);
+        setDesiredDate(formattedDate);
+      }
+      if (data.EndDate) {
+        const [day, month, year] = data.EndDate.split("/");
+        const formattedDate = new Date(`${year}-${month}-${day}`);
+        setEndDate(formattedDate);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchCalendar();
+  }, []);
 
   const OnclickPay = (e) => {
     e.preventDefault();
     if (!PickupDate) {
-      setNull("Vui lòng chọn ngày trả xe");
+      setNull("Vui lòng chọn ngày nhận xe");
     } else if (!ReturnDate) {
       setNull("Vui lòng chọn ngày trả xe");
     }
     if (!PickupDate && !ReturnDate) {
-      setNull("Vui lòng chọn ngày nhận và trả xe");
-    } else if (selectedPickUpDate < today) {
-      setNull("Ngày nhận xe không hợp lệ");
-    } else if (selectedReturnDate < today) {
-      setNull("Ngày trả xe không hợp lệ");
-    } else {
+      setNull2("Vui lòng chọn ngày nhận và trả xe");
+    } else if (ReturnDate && PickupDate && PickupDate < ReturnDate) {
       navigate(`/Payment/${id}`, {
         state: {
           Pickup_Date: formatDate(PickupDate),
@@ -77,54 +174,16 @@ const Reservation = () => {
     }
   };
 
+  useEffect(() => {
+    DetailFetch();
+  }, [id]);
+
   const formattedPrice = (price) => {
     return new Intl.NumberFormat("vi-VN", {
       style: "currency",
       currency: "VND",
     }).format(price);
   };
-
-  const fetchCalculate = async () => {
-    try {
-      const response = await fetch(`${URL}/CalculateContractPrice`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          Pickup_Date: PickupDate,
-          Return_Date: ReturnDate,
-          MaVehicle: id,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      const data = await response.json();
-      setTotal(data);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  useEffect(() => {
-    if (ReturnDate && selectedReturnDate < today) {
-      setNull("Ngày trả xe không hợp lệ");
-      setReturnDate(null);
-      setTotal(0);
-    } else if (PickupDate && selectedPickUpDate < today) {
-      setNull("Ngày nhận xe không hợp lệ");
-      setPickUpDate(null);
-      setTotal(0);
-    } else if (ReturnDate && PickupDate) {
-      setNull(null);
-      fetchCalculate();
-    }
-  }, [PickupDate, ReturnDate]);
-
-  useEffect(() => {
-    DetailFetch();
-  }, [id]);
 
   if (loading) {
     return (
@@ -135,8 +194,16 @@ const Reservation = () => {
   }
   if (error) {
     return (
-      <div className="text-center w-full text-4xl translate-y-1/2 h-full font-extrabold">
-        {error}
+      <div id="error-page" className="h-screen translate-y-1/3">
+        <h1 className="text-center text-6xl text-red-600 font-extrabold">
+          Oops!
+        </h1>
+        <p className="text-center text-3xl pt-5">
+          Sorry, an unexpected error has occurred.
+        </p>
+        <p className="text-center text-3xl">
+          <i>{error}</i>
+        </p>
       </div>
     );
   }
@@ -168,19 +235,18 @@ const Reservation = () => {
           />
           <div className="grid grid-cols-3 grid-rows-1 lg:grid-cols-1 lg:grid-rows-3 gap-4 w-full">
             {vehicle.imageVehicle && vehicle.imageVehicle.length > 0 ? (
-              vehicle.imageVehicle.map(
-                (img, index) =>
-                  index === 1 ? (
-                    <img
-                      key={index}
-                      src={img}
-                      alt="Vehicle Image"
-                      className="w-full h-64 rounded-xl object-cover"
-                    />
-                  ) : null // Return null for indices that are not 1
+              vehicle.imageVehicle.map((img, index) =>
+                index === 1 ? (
+                  <img
+                    key={index}
+                    src={img}
+                    alt="Vehicle Image"
+                    className="w-full h-64 rounded-xl object-cover"
+                  />
+                ) : null
               )
             ) : (
-              <p>Không có ảnh</p>
+              <span>Không có ảnh</span>
             )}
           </div>
         </div>
@@ -295,14 +361,30 @@ const Reservation = () => {
                     <div className="col-span-5">
                       <label className="font-bold">Nhận xe</label>
                     </div>
-                    <div className="col-span-12 w-full">
-                      <input
-                        className="border-2 border-[#75bde0] outline-none text-[#3b7097] placeholder:text-[#75bde0] px-2 py-2 h-full w-full rounded-lg bg-[#ffffff]"
-                        type="date"
-                        value={PickupDate}
-                        onChange={(e) => setPickUpDate(e.target.value)}
-                      />
-                      {Null && Null == "Ngày nhận xe không hợp lệ" && (
+                    <div
+                      className="col-span-12 mt-3 w-full"
+                      onClick={togglePickupCalendar}
+                    >
+                      <span className="border-2 border-[#75bde0] outline-none text-[#3b7097] placeholder:text-[#75bde0] py-[0.65rem] pr-14 px-2 h-full w-full rounded-lg bg-[#ffffff]">
+                        {PickupDate ? (
+                          <span>{formatDate(PickupDate)}</span>
+                        ) : (
+                          <span>Chọn ngày</span>
+                        )}
+                        {showPickupCalendar && (
+                          <div className="absolute mt-6 right-40 z-50 bg-[#ffffff] rounded-lg shadow-xl shadow-[#75bde0] p-4">
+                            <Calendar
+                              onChange={handlPickupDateChange}
+                              value={PickupDate}
+                              minDate={StartDate}
+                              tileDisabled={({ date }) =>
+                                date >= DesiredDate && date <= EndDate
+                              }
+                            />
+                          </div>
+                        )}
+                      </span>
+                      {Null && Null == "Vui lòng chọn ngày nhận xe" && (
                         <p className="text-red-500">{Null}</p>
                       )}
                     </div>
@@ -314,22 +396,39 @@ const Reservation = () => {
                       <label className="font-bold">Trả xe</label>
                     </div>
                     <div className="col-span-12">
-                      <input
-                        placeholder="Nhập số lượng"
-                        className="border-2 border-[#75bde0] outline-none text-[#3b7097] placeholder:text-[#75bde0] px-2 py-2 h-full w-full rounded-lg bg-[#ffffff]"
-                        type="date"
-                        value={ReturnDate}
-                        onChange={(e) => setReturnDate(e.target.value)}
-                      />
-                      {Null && Null == "Ngày trả xe không hợp lệ" && (
-                        <p className="text-red-500">{Null}</p>
-                      )}
+                      <div
+                        className="col-span-12 mt-3 w-full"
+                        onClick={toggleReturnCalendar}
+                      >
+                        <span className="border-2 border-[#75bde0] outline-none text-[#3b7097] placeholder:text-[#75bde0] py-[0.65rem] pr-14 px-2 h-full w-full rounded-lg bg-[#ffffff]">
+                          {ReturnDate ? (
+                            <span>{formatDate(ReturnDate)}</span>
+                          ) : (
+                            <span>Chọn ngày</span>
+                          )}
+                          {showReturnCalendar && (
+                            <div className="absolute mt-6 right-40 z-50 bg-[#ffffff] rounded-lg shadow-xl shadow-[#75bde0] p-4">
+                              <Calendar
+                                onChange={handleReturnDateChange}
+                                value={ReturnDate}
+                                minDate={StartDate}
+                                tileDisabled={({ date }) =>
+                                  date >= DesiredDate && date <= EndDate
+                                }
+                              />
+                            </div>
+                          )}
+                        </span>
+                        {Null && Null == "Vui lòng chọn ngày trả xe" && (
+                          <p className="text-red-500">{Null}</p>
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-              {Null && Null == "Vui lòng chọn ngày nhận và trả xe" && (
-                <p className="text-red-500 mt-6 w-full text-center">{Null}</p>
+              {Null2 && (
+                <p className="text-red-500 mt-6 w-full text-center">{Null2}</p>
               )}
               <div className="mt-10 rounded-lg shadow-xl shadow-[#75bde0] p-4">
                 <p>Địa điểm giao xe</p>
